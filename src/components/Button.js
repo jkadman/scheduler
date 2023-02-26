0fa1ef2d4557fedd06ea327283435db33877ad5c
Jason Kadman <jkadman231@gmail.com>
2023-02-25 18:37:48 -0800

    clean up code

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,27 +1,15 @@
 import React from "react";
-import { useState } from "react";
+// import { useState } from "react";
 import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
 
-   // if (props.confirm) {
-   //    buttonClass += " button--confirm";
-   //    console.log('buttonClass:', buttonClass)
-   // }
-
    const buttonClass = classNames("button", {
       "button--confirm": props.confirm,
       "button--danger": props.danger
     });
-
-   // classNames('props.confirm', {buttonClass:'button--confirm'});
-   // if (props.danger) {
-   //    buttonClass += " button--danger";
-   // }
-   // classNames('props', {danger: true});
-   
 
     return (
 
