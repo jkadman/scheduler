d9443c6671779ed5dabab83701a961ed067b1231
Jason Kadman <jkadman231@gmail.com>
2023-02-25 18:38:40 -0800

    finish Interview List setup

---
+++ src/components/InterviewerListItem.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import classNames from 'classnames';
+import "components/InterviewerListItem.scss"
+
+const InterviewerListItem = (props) => {
+  // console.log('ULI:', props)
+  // console.log('setInterviewer', props.setInterviewer)
+
+  let interviewerClass = classNames({
+    "interviewers__item": true,
+    "interviewers__item--selected": props.selected
+  })
+
+  const selectedName = () => {
+    if (props.selected) {
+      return `${props.name}`
+    }
+  }
+
+  return (
+    <li className={interviewerClass}
+      onClick={() => props.setInterviewer(props.id)}
+    >
+      <img
+        className={interviewerClass}
+        src={props.avatar}
+        name={props.name}
+      />
+      {selectedName()}
+    </li>
+  )
+}
+
+export default InterviewerListItem;
