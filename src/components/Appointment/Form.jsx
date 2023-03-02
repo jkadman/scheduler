dd4021a52ceecf762847706003dce7dac240cd36
Jason Kadman <jkadman231@gmail.com>
2023-03-02 07:28:51 -0800

    create Appointment Form

---
+++ src/components/Appointment/Form.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
+
+const Form = (props) => {
+  console.log('formprops', props)
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            onChange={(event) => setStudent(event.target.value)}
+          />
+        </form>
+        <InterviewerList
+        /* your code goes here */
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger  >Cancel</Button>
+          <Button confirm  >Save</Button>
+        </section>
+      </section>
+    </main>
+  )
+}
+
+export default Form;
