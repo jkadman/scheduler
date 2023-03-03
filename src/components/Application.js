7be034e20bec9ef903b5c494798ef70536b302fc
Jason Kadman <jkadman231@gmail.com>
2023-03-03 17:02:00 +0000

    add ability to access hardcoded data via props

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "./Appointment";
 
 const days = [
   {
@@ -21,11 +22,60 @@ const days = [
   },
 ];
 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
 
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday")
- console.log(day)
+  const appointmentData = Object.values(appointments).map((element) => {
+    console.log('AppointmentData', element)
+    return (
+      <Appointment
+        key={element.id}
+        {...element}
+        
+      />
+    )
+  })
+  console.log(appointmentData)
   return (
     <main className="layout">
       <section className="sidebar">
@@ -49,7 +99,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentData} 
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
