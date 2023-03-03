ea9d5874eb316cfa97c32e240c2abf0a139a1f07
Jason Kadman <jkadman231@gmail.com>
2023-03-03 13:17:25 -0800

    access days from API instead of hardcoded data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -65,6 +48,14 @@ const appointments = {
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday")
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days").then((response) => {
+      setDays([...response.data]);
+    })
+  }, [])
+
   const appointmentData = Object.values(appointments).map((element) => {
     console.log('AppointmentData', element)
     return (
