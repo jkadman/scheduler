34156907b0b26037a3e9d9904ccf33a3675c065f
Jason Kadman <jkadman231@gmail.com>
2023-03-09 12:19:18 -0800

    refactor functions

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,30 +4,21 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode, replace = false) => {
-    if (replace) {
-      history.shift
-    } else {
-      setHistory(prev => [...prev, newMode]);
-    }
-    console.log('newMode', newMode)
-    setMode(newMode);
-    console.log('mode', mode)
-  }
+  const transition = (mode, replace = false) => {
+    setHistory(prev =>
+      replace ? [...prev.slice(0, -1), mode] : [...prev, mode]
+    );
+  }; 
 
   //When transition is called, we need to add the new mode to our history.
 //When back is called, we should set the mode to the previous item in our history array.
 // 
 
-  const back = () => { 
-    if (history.length > 1) {
-    const newHistory = history;
-    newHistory.pop()
-    setHistory(newHistory);
-    setMode(newHistory[newHistory.length - 1]);
-    }
-  }
+const back = () => {
+  setHistory(prev => (prev.length > 1 ? prev.slice(0, -1) : prev));
+}; 
 
-  return { mode, transition, back }
+  return { mode: history[history.length - 1], transition, back }; 
 }
 
+// ['empty']
