2df31961626b1204e65d307451df771b9aa7f3a2
Jason Kadman <jkadman231@gmail.com>
2023-03-19 22:43:50 -0700

    add updatespots function

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -8,7 +8,7 @@ export default function useApplicationData() {
     appointments: {},
     interviewers: {}
   });
-
+  console.log('applength', setState.appointments)
   const setDay = day => setState({ ...state, day });
 
   useEffect(() => {
@@ -21,18 +21,28 @@ export default function useApplicationData() {
     })
   }, [])
 
+
+
+  useEffect(() => {
+    updateSpots(state.days, state.appointments);
+  }, [state.appointments]);
+
   function bookInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview }
     };
-    console.log('appointmentBI', appointment)
     const appointments = {
       ...state.appointments,
       [id]: appointment
     };
+    console.log('bookapp', appointments)
     return axios.put(`/api/appointments/${id}`, {interview})
-      .then(res => {setState({...state, appointments})});   
+      .then(() => {
+        setState({...state, appointments})
+
+        
+    });   
   } 
   
   function cancelInterview(id) {
@@ -45,8 +55,42 @@ export default function useApplicationData() {
       [id]: appointment
     }
     return axios.delete(`/api/appointments/${id}`)
-      .then(res => setState({...state, appointments}))    
+      .then(() => { 
+        setState({...state, appointments})
+      })
   };
+
+  
+  const updateSpots = (days, appointments) => {
+    const updatedDays = days.map(day => {
+      const spots = day.appointments.reduce((acc, curr) => {
+        if (!appointments[curr].interview) {
+          return acc + 1;
+        }
+        return acc;
+      }, 0);
+      return { ...day, spots };
+    });
+  
+    setState(prev => ({ ...prev, days: updatedDays }));
+  };
+  
+  
+
+  // }
+  // number of spots inside day object
+  // number available relates to appointments that dont have an interview booked
+  // we should update the spots object when we cancel or book an interview
+  // what type or data is spots, what is spots initial state?  A value of an object key
+
+  // updateSpots, if updatespots is true, - 1 from spots.  If updateSpots is false, +1 to spots
+  // access the spots object before setting state, much like with appointments in the bookInterview function
+
+  // to update spots place .then in bookInterview (spots - 1) and cancelInterview (spot + 1)
+  // if appointment is null spots = appointment
+
+  // connection between daylist and appointments, is that days has an array of what appointments correspond to what interviews
+  // for each appointment[element.appointments] {if (appointment[element.appointments = null ]) {days.spots++}}
 
   return {
     state,
@@ -55,3 +99,4 @@ export default function useApplicationData() {
     cancelInterview
   };
 }
+
