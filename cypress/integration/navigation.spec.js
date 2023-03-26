23d93721602469edc5b8c0364624178d5495a2ba
Jason Kadman <jkadman231@gmail.com>
2023-03-25 18:26:57 -0700

    add Cypress and EtE tests

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -3,6 +3,8 @@ describe("Navigation", () => {
     cy.visit("/");
   });
   it("should navigate to Tuesday", () => {
-    cy.visit("/")
+    cy.contains("[data-testid=day]", "Tuesday")
+  .click()
+  .should("have.class", "day-list__item--selected");
   })
 });
